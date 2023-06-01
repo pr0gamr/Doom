d53b8d8608ac65e9dc792ba61653b015c2521e86
Matthew Ertman <113724461+pr0gamr@users.noreply.github.com>
2023-06-01 11:36:27 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,62 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "{wI3W#S88KkC/+v=6":
+            case "Bullet":return img`
+5 5 
+5 5 
+`;
+            case "fIh/]+[R1b]GHTlMiy_L":
+            case "Enemy":return img`
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`;
+            case "GT|b$hDCoT2HM123KDDM":
+            case "E":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . c a a a c . . . . . . . 
+. . . c c f a b b c . . . . . . 
+. . . b f f b f a a . . . . . . 
+. . . b b a b f f a . . . . . . 
+. . . c b f b b a c . . . . . . 
+. . . . b a f c c . . . . . . . 
+. . . . . b b c . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.jumpWithHeightAndDuration(Render.getRenderSpriteInstance(), 16, 500)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        5 5 
-        5 5 
-        `, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 50, Render.getAttribute(Render.attribute.dirY) * 50)
+    projectile = sprites.createProjectileFromSprite(assets.image`Bullet`, Render.getRenderSpriteInstance(), Render.getAttribute(Render.attribute.dirX) * 100, Render.getAttribute(Render.attribute.dirY) * 100)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+})
+info.onCountdownEnd(function () {
+    game.setGameOverMessage(false, "YOU RAN OUT OF TIME")
+    game.gameOver(false)
 })
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.toggleViewMode()
@@ -13,22 +20,33 @@ controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.fire, 500)
     sprites.destroy(sprite)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-1)
 })
 let projectile: Sprite = null
+game.setGameOverMessage(false, "YOU DIED")
+game.setGameOverMessage(true, "YOU BEAT THE INVASION")
+let spawners = 5
 tiles.setCurrentTilemap(tilemap`level1`)
 Render.setViewMode(ViewMode.raycastingView)
 tiles.placeOnRandomTile(Render.getRenderSpriteInstance(), sprites.builtin.forestTiles10)
-let mySprite = sprites.create(img`
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 
-    `, SpriteKind.Enemy)
+let mySprite = sprites.create(assets.image`Enemy`, SpriteKind.Enemy)
 mySprite.follow(Render.getRenderSpriteInstance(), 20)
-tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles10)
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
+info.setLife(5)
+game.showLongText("PRESS A WHEN READY", DialogLayout.Full)
+info.startCountdown(180)
+forever(function () {
+    if (spawners == 0) {
+        game.gameOver(true)
+    }
+})
+forever(function () {
+    pause(2000)
+    mySprite = sprites.create(assets.image`Enemy`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
+    mySprite.follow(Render.getRenderSpriteInstance(), 20)
+})
